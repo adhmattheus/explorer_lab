9bf80cd11fbfac12dddf148675480081f73b9181
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-10-24 11:07:56 -0300

    expressoes regulares para os cartoes

--- src/main.js
+++ src/main.js
@@ -43,3 +43,23 @@ const expirationDatePattern = {
   }
 }
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.querySelector('#card-number')
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}| 22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ]
+}
