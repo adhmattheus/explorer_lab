59b6325b87cfb0956a6ba001eaf18b01b5126626
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-10-24 10:20:18 -0300

    máscaras de dados de entrada

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import IMask from 'imask';
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
@@ -15,7 +16,30 @@ function setCardType(type) {
   ccBgColor02.setAttribute("fill", colors[type][1])
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
-
 // setCardType("visa")
-
 globalThis.setCardType = setCardType
+
+//security code
+const securityCode = document.querySelector('#security-code')
+const securityCodePattern = {
+  mask: "000"
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2)
+    }
+  }
+}
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
